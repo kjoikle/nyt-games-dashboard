2ccabc1781911c23286b7b2130bac539480f4e78
kjoikle <60455201+kjoikle@users.noreply.github.com>
2024-12-16 16:55:00 -0500

    deployed first cloud function

---
+++ functions/src/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// import { onRequest } from 'firebase-functions/v2/https';
+// import * as logger from 'firebase-functions/logger';
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info('Hello logs!', { structuredData: true });
+//   response.send('Hello from Firebase!');
+// });
+
+import * as v1 from 'firebase-functions/v1';
+
+export const newUserSignUp = v1.auth.user().onCreate((user) => {
+  console.log('new user: ', user.email);
+});
