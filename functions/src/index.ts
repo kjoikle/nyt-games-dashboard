5b3df294f9e36907a0536404eff4259a62583417
kjoikle <60455201+kjoikle@users.noreply.github.com>
2024-12-18 11:52:58 -0500

    basic cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,29 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// import { onRequest } from 'firebase-functions/v2/https';
-// import * as logger from 'firebase-functions/logger';
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info('Hello logs!', { structuredData: true });
-//   response.send('Hello from Firebase!');
-// });
-
 import * as v1 from 'firebase-functions/v1';
+import * as v2 from 'firebase-functions/v2';
+import admin from 'firebase-admin';
+admin.initializeApp();
+const db = admin.firestore();
 
 export const newUserSignUp = v1.auth.user().onCreate((user) => {
-  console.log('new user: ', user.email);
+  return db.collection('users').doc(user.uid).set({
+    email: user.email,
+    displayName: null,
+  });
 });
+
+export const userDeleted = v1.auth.user().onDelete((user) => {
+  const doc = db.collection('users').doc(user.uid);
+  return doc.delete();
+});
+
+// export const addGame = v2.https.onCall((req) => {
+//   if (!req.auth) {
+//     throw new v2.https.HttpsError(
+//       'unauthenticated',
+//       'only authenticated users can update data',
+//     );
+//   }
+// other checks -- determine if connections data or strands and update global params
+
+// db.collection('users').doc(req.auth.uid).collection('games').add
+// });
